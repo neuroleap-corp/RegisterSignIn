149361cee27adacecfabf86163d4d8f542c70ae4
Babur Farooq <babur.farooq@gmail.com>
2021-05-30 15:31:58 -0400

    Initial Commit

---
+++ RegisterSignIn/RegisterSignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterSignInApp.swift
+//  RegisterSignIn
+//
+//  Created by Babur Farooq on 5/30/21.
+//
+
+import SwiftUI
+
+@main
+struct RegisterSignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
