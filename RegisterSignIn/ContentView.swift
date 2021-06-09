59d6ec2f086a917a0709c8440badbac70b40ff6d
Babur Farooq <babur.farooq@gmail.com>
2021-06-08 21:14:17 -0400

    ots2_swiftUI

--- RegisterSignIn/ContentView.swift
+++ RegisterSignIn/ContentView.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack{
+                Text ("Welcome To NeuroLeap")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.orange)
+                Spacer()
+                
+                NavigationLink(
+                    destination:  UserAndPassword()) {
+                    Text("Sign In")
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,  maxWidth: 300)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(40)
+                        .font(.title)
+                }
+                NavigationLink(
+                    destination:  UserAndPassword()) {
+                    Text("Register")
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/,  maxWidth: 300)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(40)
+                        .font(.title)
+                }
+                Spacer()
+            }
+            
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
